6aacd05fcbd290716de033ece676557bf64e35c1
rahul shinde <rshinde@mica.edu>
2015-08-05 17:41:50 -0600

    testing loads

--- js/load.js
+++ js/load.js
@@ -1,5 +1,11 @@
 $(document).ready( function() {
-    $("#link").on("click", function() {
-        $("#content").load("test.html");
+	$("#content").load("intro.html");
+
+    $("#intro-link").on("click", function() {
+        $("#content").load("intro.html");
+    });
+
+    $("#logo-link").on("click", function() {
+        $("#content").load("logo.html");
     });
 });
