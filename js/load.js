9a36fd92a19356901c8933810b4a6544da023098
rahul shinde <rshinde@mica.edu>
2015-08-05 18:14:43 -0600

    adding underlines and completing load.js

--- js/load.js
+++ js/load.js
@@ -3,9 +3,37 @@ $(document).ready( function() {
 
     $("#intro-link").on("click", function() {
         $("#content").load("intro.html");
+        $('.link').removeClass('active');
+		$(this).addClass('active');
     });
 
     $("#logo-link").on("click", function() {
         $("#content").load("logo.html");
+        $('.link').removeClass('active');
+		$(this).addClass('active');
+    });
+
+    $("#invite-link").on("click", function() {
+        $("#content").load("invite.html");
+        $('.link').removeClass('active');
+		$(this).addClass('active');
+    });
+
+    $("#mes-link").on("click", function() {
+        $("#content").load("mes.html");
+        $('.link').removeClass('active');
+		$(this).addClass('active');
+    });
+
+    $("#4d-link").on("click", function() {
+        $("#content").load("4d.html");
+        $('.link').removeClass('active');
+		$(this).addClass('active');
+    });
+
+    $("#td-link").on("click", function() {
+        $("#content").load("td.html");
+        $('.link').removeClass('active');
+		$(this).addClass('active');
     });
 });
