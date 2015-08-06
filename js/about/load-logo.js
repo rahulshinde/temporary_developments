253dcb63fb9e813238b8c1a5918241e264fe8bd6
rahul shinde <rshinde@mica.edu>
2015-08-06 00:04:35 -0600

    loads with enquire, one break-point

--- js/about/load-logo.js
+++ js/about/load-logo.js
@@ -10,21 +10,37 @@ init();
 
 function init() {
     large = Math.floor(Math.random() * 5) + 1 
-    largeText = '<img class="logo-large" src="img/about/logo-large/logo' + large +'.jpg" alt="SOPH logo"/>'
+    largeText = '<img class="logo" src="img/about/logo-large/logo' + large +'.jpg" alt="SOPH logo"/>'
+
+    med = Math.floor(Math.random() * 3) + 1 
+    medText = '<img class="logo" src="img/about/logo-med/logo' + med +'.jpg" alt="SOPH logo"/>'
 
     placeImg();
 }
 
 function placeImg() {
-    enquire.register("screen and (min-width: 750px)", {
+    enquire.register("screen and (min-width: 900px)", {
         setup : function() {
-            $('#load-large').append(largeText);
+            $('#load-large').append(largeText).hide();
         },    
         match : function() {
             $('#load-large').fadeIn('slow', function () {} );
         },  
         unmatch : function() {
-            $('#load-large').fadeOut('fast', function () {} );
+            $('#load-large').hide();
+        }
+    });
+
+    enquire.register("screen and (max-width: 899px)", {
+        setup : function() {
+            console.log('hello');
+            $('#load-med').append(medText).hide();
+        },    
+        match : function() {
+            $('#load-med').fadeIn('slow', function () {} );
+        },  
+        unmatch : function() {
+            $('#load-med').hide();
         }
     });
 }
