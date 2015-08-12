7f16513f87e6e18aa1ad56c0f275cc1d5b94adea
rahul shinde <rshinde@mica.edu>
2015-08-12 11:49:48 -0600

    shifting loading to be only one request

--- js/load.js
+++ js/load.js
@@ -155,4 +155,16 @@ $(document).ready( function() {
         }
     });
 
+    // mobile nav loading
+
+    $("#nav-open").on("click", function() {
+        $("#nav-open").fadeToggle("fast", function() {});
+        $(".mobile-nav").slideToggle("slow", function() {});       
+    });
+
+    $("#close").on("click", function() {
+        $(".mobile-nav").slideToggle("fast", function() {});
+        $("#nav-open").fadeToggle("slow", function() {});
+    });
+
 });
