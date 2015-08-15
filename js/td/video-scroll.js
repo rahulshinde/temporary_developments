87d35b462b9c1a86722c6578158bade73458b68b
rahul shinde <rshinde@mica.edu>
2015-08-14 18:19:58 -0600

    adding conditions for video scrubbing

--- js/td/video-scroll.js
+++ js/td/video-scroll.js
@@ -5,15 +5,17 @@ function updateVideo() {
     var scrollPosition = $('#scroll').scrollTop();
     var scrollY = $('#scroll')[0].scrollHeight;
 
-    video.currentTime = (scrollPosition / (scrollY - 500)) * videoLength;
-    console.log(video.currentTime);
-
     if (video.currentTime > videoLength) {
     	video.currentTime = videoLength;
     }
 
     else if (video.currentTime < 0) {
-    	console.log('under');
+    	video.currentTime = 0;
+    }
+
+    else {
+    	video.currentTime = (scrollPosition / (scrollY - 500)) * videoLength;
+    	console.log(video.currentTime);
     }
 }
 
