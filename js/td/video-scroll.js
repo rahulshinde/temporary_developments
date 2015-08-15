56a2fdb9501b397e44097870415a681ec1c76848
rahul shinde <rshinde@mica.edu>
2015-08-14 19:25:17 -0600

    if unnecessary, problem w/ video

--- js/td/video-scroll.js
+++ js/td/video-scroll.js
@@ -5,18 +5,10 @@ function updateVideo() {
     var scrollPosition = $('#scroll').scrollTop();
     var scrollY = $('#scroll')[0].scrollHeight;
 
-    if (video.currentTime > videoLength) {
-    	video.currentTime = 109.22;
-    }
+    console.log(videoLength);
 
-    else if (video.currentTime < 0) {
-    	video.currentTime = 0;
-    }
-
-    else {
-    	video.currentTime = (scrollPosition / (scrollY - 500)) * videoLength;
-    	console.log(video.currentTime);
-    }
+    video.currentTime = (scrollPosition / (scrollY - 500)) * videoLength;
+    console.log(video.currentTime);
 }
 
 $('#scroll').scroll(function () {
