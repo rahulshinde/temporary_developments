8e75aa87019f8f0a5f73f093e45f5d47ee4d95df
rahul shinde <rshinde@mica.edu>
2015-09-02 17:26:13 -0400

    need to make some changes to load function for no-repeats

--- js/about/load-logo.js
+++ js/about/load-logo.js
@@ -1,1 +1,61 @@
-function init(){large=Math.floor(5*Math.random())+1,largeText='<img class="logo" src="img/about/logo-large/logo'+large+'.jpg" alt="SOPH logo"/>',med=Math.floor(5*Math.random())+1,medText='<img class="logo" src="img/about/logo-med/logo'+med+'.jpg" alt="SOPH logo"/>',small=Math.floor(5*Math.random())+1,smallText='<img class="logo" src="img/about/logo-small/logo'+small+'.jpg" alt="SOPH logo"/>',placeImg()}function placeImg(){enquire.register("screen and (min-width: 975px)",{setup:function(){$("#load-large").append(largeText).hide()},match:function(){$("#load-large").show()},unmatch:function(){$("#load-large").hide()}}),enquire.register("screen and (min-width:650px) and (max-width: 974px)",{setup:function(){$("#load-med").append(medText).hide()},match:function(){$("#load-med").show()},unmatch:function(){$("#load-med").hide()}}),enquire.register("screen and (max-width: 649px)",{setup:function(){$("#load-small").append(smallText).hide()},match:function(){$("#load-small").show(),$("#load-med").hide()},unmatch:function(){$("#load-small").hide()}})}var large,med,small,largeText,medText,smallText;init();
+var large,
+    med,
+    small;
+
+var largeText,
+    medText,
+    smallText;
+
+init();
+
+function init() {
+    large = Math.floor(Math.random() * 5) + 1 
+    largeText = '<img class="logo" src="img/about/logo-large/logo' + large +'.jpg" alt="SOPH logo"/>'
+
+    med = Math.floor(Math.random() * 5) + 1 
+    medText = '<img class="logo" src="img/about/logo-med/logo' + med +'.jpg" alt="SOPH logo"/>'
+
+    small = Math.floor(Math.random() * 5) + 1 
+    smallText = '<img class="logo" src="img/about/logo-small/logo' + small +'.jpg" alt="SOPH logo"/>'
+
+    placeImg();
+}
+
+function placeImg() {
+    enquire.register("screen and (min-width: 975px)", {
+        setup : function() {
+            $('#load-large').append(largeText).hide();
+        },    
+        match : function() {
+            $('#load-large').show();
+        },  
+        unmatch : function() {
+            $('#load-large').hide();
+        }
+    });
+
+    enquire.register("screen and (min-width:650px) and (max-width: 974px)", {
+        setup : function() {
+            $('#load-med').append(medText).hide();
+        },    
+        match : function() {
+            $('#load-med').show();
+        },  
+        unmatch : function() {
+            $('#load-med').hide();
+        }
+    });
+
+    enquire.register("screen and (max-width: 649px)", {
+        setup : function() {
+            $('#load-small').append(smallText).hide();
+        },    
+        match : function() {
+            $('#load-small').show();
+            $('#load-med').hide();
+        },  
+        unmatch : function() {
+            $('#load-small').hide();
+        }
+    });
+}
